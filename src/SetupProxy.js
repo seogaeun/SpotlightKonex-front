da79e74652050ceee2eb2801169ee4cae0908f8e
seogaeun <se0323333@gmail.com>
2023-11-21 20:51:15 +0900

    fix: 프록시 주석 처리

--- src/SetupProxy.js
+++ src/SetupProxy.js
@@ -1,12 +1,12 @@
-const {createProxyMiddleware} = require('http-proxy-middleware')
+// const {createProxyMiddleware} = require('http-proxy-middleware')
 
-module.exports = app => {
-    app.use('/proxy',
-        createProxyMiddleware(
-            {
-                target: process.env.REACT_APP_API_URL,
-                changeOrigin: true,
-            }
-        )
-    )
-}
+// module.exports = app => {
+//     app.use('/proxy',
+//         createProxyMiddleware(
+//             {
+//                 target: process.env.REACT_APP_API_URL,
+//                 changeOrigin: true,
+//             }
+//         )
+//     )
+// }
