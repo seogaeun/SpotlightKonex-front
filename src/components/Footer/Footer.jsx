7dcd9883ac7e3b4b2df24460ea291c17a81e7a0f
Seoyun0626 <tjdbs0626@gmail.com>
2023-11-22 09:48:14 +0900

    [feat] footer 크기 변경
    [feat] footer 크기 변경

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,10 +6,10 @@ import "./style.css";
 export const Footer = () => {
   return (
     <div className="footer">
-      <p className="footer-bold">서비스</p>
-      <a href="https://drive.google.com/file/d/1khmX20vWSmmrneJAWHBuB8P6HQZvv4hd/view?usp=sharing">서비스 이용약관</a>
-      <a href="https://minseoo.notion.site/0b8456467f9d4d1b834df3ab75040f8a">서비스 소개</a>
-      <p>&copy; 2023 Spotlight KONEX. All rights reserved.</p>
+      <p class="footer-thin">서비스</p>
+      <a class="footer-bold" href="https://drive.google.com/file/d/1khmX20vWSmmrneJAWHBuB8P6HQZvv4hd/view?usp=sharing">서비스 이용약관</a>
+      <a class="footer-bold" href="https://minseoo.notion.site/0b8456467f9d4d1b834df3ab75040f8a">서비스 소개</a>
+      <p class="footer-thin">&copy; 2023 Spotlight KONEX. All rights reserved.</p>
     </div>
   );
 };
